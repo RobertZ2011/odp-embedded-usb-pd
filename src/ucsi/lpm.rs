dc91a4e6de4ff083305be1ceb6ed81d2d0e77410
Adam Sasine <adam.sasine@gmail.com>
2025-03-27 10:13:37 -0400

    Copy UCSI module from embedded_services::type_c::ucsi
    See https://github.com/OpenDevicePartnership/embedded-services/issues/197

---
+++ src/ucsi/lpm.rs
@@ -0,0 +1,33 @@
+use crate::{GlobalPortId, PdError};
+
+/// Connector reset types
+#[derive(Copy, Clone, Debug)]
+#[cfg_attr(feature = "defmt", derive(defmt::Format))]
+pub enum ResetType {
+    Hard,
+    Data,
+}
+
+/// LPM command data
+#[derive(Copy, Clone, Debug)]
+#[cfg_attr(feature = "defmt", derive(defmt::Format))]
+pub enum CommandData {
+    ConnectorReset(ResetType),
+}
+
+/// LPM commands
+#[derive(Copy, Clone, Debug)]
+#[cfg_attr(feature = "defmt", derive(defmt::Format))]
+pub struct Command {
+    pub port: GlobalPortId,
+    pub operation: CommandData,
+}
+
+/// LPM response data
+#[derive(Copy, Clone, Debug)]
+#[cfg_attr(feature = "defmt", derive(defmt::Format))]
+pub enum ResponseData {
+    Complete,
+}
+
+pub type Response = Result<ResponseData, PdError>;
