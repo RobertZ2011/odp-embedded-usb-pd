d62451c0e7bf2121c51c03e70029830041a31c33
RobertZ2011 <33537514+RobertZ2011@users.noreply.github.com>
2025-07-29 09:26:49 -0700

    Add source transition request constants (#29)

---
+++ src/constants.rs
@@ -0,0 +1,6 @@
+//! Constants for USB Power Delivery (USB PD) protocol
+
+/// Source transition request time in milliseconds for SPR mode
+pub const T_SRC_TRANS_REQ_SPR_MS: u16 = 325;
+/// Source transition request time in milliseconds for EPR mode
+pub const T_SRC_TRANS_REQ_EPR_MS: u16 = 760;
