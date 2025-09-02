1718c8c9d68523c5ec5494fa4193eda639185e5f
Robert Zieba <robertzieba@microsoft.com>
2025-09-02 15:57:54 -0700

    ucsi/lpm: Add additional command types
    Add types for `SET_POWER_LEVEL`, `SET_NEW_CAM`, `GET_ERROR_STATUS`

---
+++ src/ucsi/lpm/get_error_status.rs
@@ -0,0 +1,294 @@
+use bincode::de::{Decode, Decoder};
+use bincode::enc::{Encode, Encoder};
+use bincode::error::{DecodeError, EncodeError};
+use bitfield::bitfield;
+
+use crate::ucsi::{CommandHeaderRaw, COMMAND_LEN};
+
+/// Data length for the GET_CONNECTOR_STATUS command response
+pub const RESPONSE_DATA_LEN: usize = MAX_VENDOR_DATA_LEN + size_of::<InformationRaw>();
+/// Command padding, -1 for the connector number byte
+pub const COMMAND_PADDING: usize = COMMAND_LEN - size_of::<CommandHeaderRaw>() - 1;
+
+/// Maximum support vendor-data length
+pub const MAX_VENDOR_DATA_LEN: usize = 14;
+
+#[derive(Copy, Clone, Debug)]
+#[cfg_attr(feature = "defmt", derive(defmt::Format))]
+pub struct Args;
+
+impl Encode for Args {
+    fn encode<E: Encoder>(&self, encoder: &mut E) -> Result<(), EncodeError> {
+        // Padding to fill the command length
+        [0u8; COMMAND_PADDING].encode(encoder)
+    }
+}
+
+impl<Context> Decode<Context> for Args {
+    fn decode<D: Decoder<Context = Context>>(decoder: &mut D) -> Result<Self, DecodeError> {
+        // Read padding
+        let _padding: [u8; COMMAND_PADDING] = Decode::decode(decoder)?;
+        Ok(Self)
+    }
+}
+
+bitfield! {
+    /// Raw error bitfield
+    #[derive(Copy, Clone, PartialEq, Eq)]
+    #[cfg_attr(feature = "defmt", derive(defmt::Format))]
+    struct InformationRaw(u16);
+    impl Debug;
+
+    /// Unrecognized command
+    pub bool, unrecognized_command, set_unrecognized_command: 0;
+    /// Invalid connector number
+    pub bool, invalid_connector, set_invalid_connector: 1;
+    /// Invalid command arguments
+    pub bool, invalid_command_args, set_invalid_command_args: 2;
+    /// Incompatible partner
+    pub bool, incompatible_partner, set_incompatible_partner: 3;
+    /// CC communication error
+    pub bool, cc_comm, set_cc_com: 4;
+    /// Failed due to dead battery
+    pub bool, dead_battery, set_dead_battery: 5;
+    /// Contract negociation failure
+    pub bool, contract_failure, set_contract_failure: 6;
+    /// Overcurrent
+    pub bool, overcurrent, set_overcurrent: 7;
+    /// Undefined
+    pub bool, undefined, set_undefined: 8;
+    /// Swap rejected by port partner
+    pub bool, port_partner_rejected_swap, set_port_partner_rejected_swap: 9;
+    /// Hard reset
+    pub bool, hard_reset, set_hard_reset: 10;
+    /// PPM policy conflict
+    pub bool, ppm_policy_conflict, set_ppm_policy_conflict: 11;
+    /// Swap rejected
+    pub bool, swap_rejected, set_swap_rejected: 12;
+    /// Reverse current protection
+    pub bool, reverse_current_protection, set_reverse_current_protection: 13;
+    /// Set sink path rejected
+    pub bool, sink_path_rejected, set_sink_path_rejected: 14;
+}
+
+#[derive(Copy, Clone, Debug, PartialEq, Eq)]
+#[cfg_attr(feature = "defmt", derive(defmt::Format))]
+pub struct Information(InformationRaw);
+
+impl Information {
+    pub fn unrecognized_command(&self) -> bool {
+        self.0.unrecognized_command()
+    }
+
+    pub fn set_unrecognized_command(&mut self, value: bool) -> &mut Self {
+        self.0.set_unrecognized_command(value);
+        self
+    }
+
+    pub fn invalid_connector(&self) -> bool {
+        self.0.invalid_connector()
+    }
+
+    pub fn set_invalid_connector(&mut self, value: bool) -> &mut Self {
+        self.0.set_invalid_connector(value);
+        self
+    }
+
+    pub fn invalid_command_args(&self) -> bool {
+        self.0.invalid_command_args()
+    }
+
+    pub fn set_invalid_command_args(&mut self, value: bool) -> &mut Self {
+        self.0.set_invalid_command_args(value);
+        self
+    }
+
+    pub fn incompatible_partner(&self) -> bool {
+        self.0.incompatible_partner()
+    }
+
+    pub fn set_incompatible_partner(&mut self, value: bool) -> &mut Self {
+        self.0.set_incompatible_partner(value);
+        self
+    }
+
+    pub fn cc_comm(&self) -> bool {
+        self.0.cc_comm()
+    }
+
+    pub fn set_cc_comm(&mut self, value: bool) -> &mut Self {
+        self.0.set_cc_com(value);
+        self
+    }
+
+    pub fn dead_battery(&self) -> bool {
+        self.0.dead_battery()
+    }
+
+    pub fn set_dead_battery(&mut self, value: bool) -> &mut Self {
+        self.0.set_dead_battery(value);
+        self
+    }
+
+    pub fn contract_failure(&self) -> bool {
+        self.0.contract_failure()
+    }
+
+    pub fn set_contract_failure(&mut self, value: bool) -> &mut Self {
+        self.0.set_contract_failure(value);
+        self
+    }
+
+    pub fn overcurrent(&self) -> bool {
+        self.0.overcurrent()
+    }
+
+    pub fn set_overcurrent(&mut self, value: bool) -> &mut Self {
+        self.0.set_overcurrent(value);
+        self
+    }
+
+    pub fn undefined(&self) -> bool {
+        self.0.undefined()
+    }
+
+    pub fn set_undefined(&mut self, value: bool) -> &mut Self {
+        self.0.set_undefined(value);
+        self
+    }
+
+    pub fn port_partner_rejected_swap(&self) -> bool {
+        self.0.port_partner_rejected_swap()
+    }
+
+    pub fn set_port_partner_rejected_swap(&mut self, value: bool) -> &mut Self {
+        self.0.set_port_partner_rejected_swap(value);
+        self
+    }
+
+    pub fn hard_reset(&self) -> bool {
+        self.0.hard_reset()
+    }
+
+    pub fn set_hard_reset(&mut self, value: bool) -> &mut Self {
+        self.0.set_hard_reset(value);
+        self
+    }
+
+    pub fn ppm_policy_conflict(&self) -> bool {
+        self.0.ppm_policy_conflict()
+    }
+
+    pub fn set_ppm_policy_conflict(&mut self, value: bool) -> &mut Self {
+        self.0.set_ppm_policy_conflict(value);
+        self
+    }
+
+    pub fn swap_rejected(&self) -> bool {
+        self.0.swap_rejected()
+    }
+
+    pub fn set_swap_rejected(&mut self, value: bool) -> &mut Self {
+        self.0.set_swap_rejected(value);
+        self
+    }
+
+    pub fn reverse_current_protection(&self) -> bool {
+        self.0.reverse_current_protection()
+    }
+
+    pub fn set_reverse_current_protection(&mut self, value: bool) -> &mut Self {
+        self.0.set_reverse_current_protection(value);
+        self
+    }
+
+    pub fn sink_path_rejected(&self) -> bool {
+        self.0.sink_path_rejected()
+    }
+
+    pub fn set_sink_path_rejected(&mut self, value: bool) -> &mut Self {
+        self.0.set_sink_path_rejected(value);
+        self
+    }
+}
+
+impl Default for Information {
+    fn default() -> Self {
+        Self(InformationRaw(0))
+    }
+}
+
+impl From<u16> for Information {
+    fn from(value: u16) -> Self {
+        Self(InformationRaw(value))
+    }
+}
+
+impl From<Information> for u16 {
+    fn from(info: Information) -> Self {
+        info.0 .0 as u16
+    }
+}
+
+/// Response data
+#[derive(Copy, Clone, Debug, PartialEq, Eq)]
+#[cfg_attr(feature = "defmt", derive(defmt::Format))]
+pub struct ResponseData {
+    /// Error information
+    pub information: Information,
+    /// Vendor-specific error information
+    pub vendor: [u8; MAX_VENDOR_DATA_LEN],
+}
+
+impl Encode for ResponseData {
+    fn encode<E: Encoder>(&self, encoder: &mut E) -> Result<(), EncodeError> {
+        u16::encode(&self.information.into(), encoder)?;
+        self.vendor.encode(encoder)?;
+        Ok(())
+    }
+}
+
+impl<Context> Decode<Context> for ResponseData {
+    fn decode<D: Decoder<Context = Context>>(decoder: &mut D) -> Result<Self, DecodeError> {
+        let information = Information::from(u16::decode(decoder)?);
+        let vendor = <[u8; MAX_VENDOR_DATA_LEN]>::decode(decoder)?;
+        Ok(ResponseData { information, vendor })
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use bincode::config::standard;
+    use bincode::{decode_from_slice, encode_into_slice};
+
+    use super::*;
+
+    #[test]
+    fn test_encode_response_data() {
+        let mut bytes = [0u8; RESPONSE_DATA_LEN];
+        let expected = ResponseData {
+            information: *Information::default()
+                .set_cc_comm(true)
+                .set_dead_battery(true)
+                .set_contract_failure(true)
+                .set_overcurrent(true),
+            vendor: [
+                0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+            ],
+        };
+
+        bytes[0] = 0xf0;
+        bytes[2] = 0x01;
+
+        let (response_data, consumed): (ResponseData, usize) =
+            decode_from_slice(&bytes, standard().with_fixed_int_encoding()).unwrap();
+        assert_eq!(consumed, bytes.len());
+        assert_eq!(response_data, expected);
+
+        let mut encoded_bytes = [0u8; RESPONSE_DATA_LEN];
+        let len = encode_into_slice(expected, &mut encoded_bytes, standard().with_fixed_int_encoding()).unwrap();
+
+        assert_eq!(len, RESPONSE_DATA_LEN);
+        assert_eq!(encoded_bytes, bytes);
+    }
+}
---
+++ src/ucsi/lpm/set_power_level.rs
@@ -0,0 +1,220 @@
+//! Types for SET_POWER_LEVEL command, see UCSI spec 6.5.19
+
+use bincode::de::Decoder;
+use bincode::enc::Encoder;
+use bincode::error::{DecodeError, EncodeError};
+use bincode::{Decode, Encode};
+use bitfield::bitfield;
+
+use crate::pdo::{MA50_UNIT, MV20_UNIT, MV25_UNIT, MW1000_UNIT, MW500_UNIT};
+use crate::{type_c, PowerRole};
+
+/// Command data length
+pub const COMMAND_DATA_LEN: usize = 6;
+
+bitfield! {
+    /// Raw arguments
+    #[derive(Copy, Clone, Default, PartialEq, Eq)]
+    #[cfg_attr(feature = "defmt", derive(defmt::Format))]
+    pub(super) struct ArgsRaw([u8]);
+    impl Debug;
+
+    /// Connector number
+    pub u8, connector_number, set_connector_number: 6, 0;
+    /// Power role, 1 = source
+    pub bool, power_role, set_power_role: 7;
+    /// Max PD power, in 0.5W/1W units depending on [`lsb_control`]
+    pub u8, max_power, set_max_power: 15, 8;
+    /// Type-C current
+    pub u8, type_c_current, set_type_c_current: 18, 16;
+    /// Units for [`max_power`] and [`output_voltage`]
+    pub bool, lsb_control, set_lsb_control: 19;
+    /// Operating current in 50mA units
+    pub u8, operating_current, set_operating_current: 27, 20;
+    /// Output voltage in 20mV/25mV units depending on [`lsb_control`]
+    pub u16, output_voltage, set_output_voltage: 41, 30;
+}
+
+/// Type-C current
+#[derive(Debug, Clone, Copy, Default, PartialEq, Eq)]
+#[cfg_attr(feature = "defmt", derive(defmt::Format))]
+pub enum Current {
+    /// PPM default
+    #[default]
+    PpmDefault,
+    /// Type-C current
+    Current(type_c::Current),
+}
+
+/// Type-C decode error, contains the invalid value
+#[derive(Debug, Clone, Copy, Default, PartialEq, Eq)]
+#[cfg_attr(feature = "defmt", derive(defmt::Format))]
+pub struct InvalidCurrent(pub u8);
+
+impl TryFrom<u8> for Current {
+    type Error = InvalidCurrent;
+
+    fn try_from(value: u8) -> Result<Self, Self::Error> {
+        match value {
+            0x00 => Ok(Current::PpmDefault),
+            0x01 => Ok(Current::Current(type_c::Current::Current3A0)),
+            0x02 => Ok(Current::Current(type_c::Current::Current1A5)),
+            0x03 => Ok(Current::Current(type_c::Current::UsbDefault)),
+            v => Err(InvalidCurrent(v)),
+        }
+    }
+}
+
+impl From<Current> for u8 {
+    fn from(value: Current) -> Self {
+        match value {
+            Current::PpmDefault => 0x00,
+            Current::Current(type_c::Current::Current3A0) => 0x01,
+            Current::Current(type_c::Current::Current1A5) => 0x02,
+            Current::Current(type_c::Current::UsbDefault) => 0x03,
+        }
+    }
+}
+
+/// Command arguments
+#[derive(Debug, Clone, Copy, Default, PartialEq, Eq)]
+#[cfg_attr(feature = "defmt", derive(defmt::Format))]
+pub struct Args(ArgsRaw<[u8; COMMAND_DATA_LEN]>);
+
+impl Args {
+    pub fn connector_number(&self) -> u8 {
+        self.0.connector_number()
+    }
+
+    pub fn set_connector_number(&mut self, connector_number: u8) -> &mut Self {
+        self.0.set_connector_number(connector_number);
+        self
+    }
+
+    pub fn power_role(&self) -> PowerRole {
+        match self.0.power_role() {
+            true => PowerRole::Source,
+            false => PowerRole::Sink,
+        }
+    }
+
+    pub fn set_power_role(&mut self, power_role: PowerRole) -> &mut Self {
+        self.0.set_power_role(power_role == PowerRole::Source);
+        self
+    }
+
+    /// Max power in mW
+    pub fn max_power(&self) -> u16 {
+        match self.0.lsb_control() {
+            true => self.0.max_power() as u16 * MW1000_UNIT as u16,
+            false => self.0.max_power() as u16 * MW500_UNIT as u16,
+        }
+    }
+
+    pub fn type_c_current(&self) -> Current {
+        let current: Result<Current, _> = Current::try_from(self.0.type_c_current());
+        current.unwrap() // Won't panic, validated in try_from
+    }
+
+    pub fn set_type_c_current(&mut self, type_c_current: Current) -> &mut Self {
+        self.0.set_type_c_current(type_c_current.into());
+        self
+    }
+
+    /// Operating current in mA
+    pub fn operating_current(&self) -> u16 {
+        self.0.operating_current() as u16 * MA50_UNIT
+    }
+
+    /// Set operating current in mA
+    pub fn set_operating_current(&mut self, operating_current: u16) -> &mut Self {
+        self.0.set_operating_current((operating_current / MA50_UNIT) as u8);
+        self
+    }
+
+    /// Output voltage in mV
+    pub fn output_voltage(&self) -> u16 {
+        match self.0.lsb_control() {
+            true => self.0.output_voltage() * MV25_UNIT,
+            false => self.0.output_voltage() * MV20_UNIT,
+        }
+    }
+
+    /// Sets LSB-control, output voltage, and max power
+    pub fn set_power_args(&mut self, lsb_control: bool, output_voltage: u16, max_power: u16) -> &mut Self {
+        self.0.set_lsb_control(lsb_control);
+
+        match lsb_control {
+            true => {
+                self.0.set_output_voltage((output_voltage / MV25_UNIT) as u16);
+                self.0.set_max_power((max_power / MW1000_UNIT as u16) as u8)
+            }
+            false => {
+                self.0.set_output_voltage((output_voltage / MV20_UNIT) as u16);
+                self.0.set_max_power((max_power / MW500_UNIT as u16) as u8);
+            }
+        }
+        self
+    }
+}
+
+impl TryFrom<[u8; COMMAND_DATA_LEN]> for Args {
+    type Error = InvalidCurrent;
+
+    fn try_from(value: [u8; COMMAND_DATA_LEN]) -> Result<Self, Self::Error> {
+        let raw = ArgsRaw(value);
+        let _current = Current::try_from(raw.type_c_current())?;
+        Ok(Self(raw))
+    }
+}
+
+impl From<Args> for [u8; COMMAND_DATA_LEN] {
+    fn from(args: Args) -> Self {
+        args.0 .0
+    }
+}
+
+impl Encode for Args {
+    fn encode<E: Encoder>(&self, encoder: &mut E) -> Result<(), EncodeError> {
+        Encode::encode(&self.0 .0, encoder)
+    }
+}
+
+impl<Context> Decode<Context> for Args {
+    fn decode<D: Decoder<Context = Context>>(decoder: &mut D) -> Result<Self, DecodeError> {
+        let raw = <[u8; COMMAND_DATA_LEN]>::decode(decoder)?;
+        Args::try_from(raw).map_err(|invalid_current| DecodeError::UnexpectedVariant {
+            type_name: "Current",
+            allowed: &bincode::error::AllowedEnumVariants::Range { min: 0, max: 3 },
+            found: invalid_current.0 as u32,
+        })
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use bincode::config::standard;
+    use bincode::decode_from_slice;
+
+    use super::*;
+
+    #[test]
+    fn test_decode_args() {
+        // Source on connector 3
+        // 1W max power
+        // USB default current
+        // 100 mA operating current
+        // 60 mV output voltage
+        let encoded: [u8; 6] = [0x83, 0x02, 0x22, 0xC0, 0x00, 0x00];
+        let (decoded, size): (Args, usize) = decode_from_slice(&encoded, standard().with_fixed_int_encoding()).unwrap();
+        assert_eq!(size, 6);
+
+        let expected = *Args::default()
+            .set_connector_number(3)
+            .set_operating_current(100)
+            .set_power_role(PowerRole::Source)
+            .set_type_c_current(Current::Current(type_c::Current::Current1A5))
+            .set_power_args(false, 60, 1000);
+        assert_eq!(decoded, expected);
+    }
+}
